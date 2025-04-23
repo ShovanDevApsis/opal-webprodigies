f1d98beb5b9fbdf5f6354fcd786542abeafdb124
ShovanDevApsis <shovan.mazumder@apsissolutions.com>
2025-04-23 18:18:11 +0600

    add

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+	// Authenticate
+
+	// if user already exists redirect them to right page
+
+	// if user does not exist create account
+	return <div>page</div>;
+};
+
+export default page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoutes = createRouteMatcher(["/dashboard(.*)", "api/payment", "/payment(.*)"]);
-export default clerkMiddleware((auth, req) => {
+const isProtectedRoutes = createRouteMatcher([
+	"/auth/sign-in(.*)",
+	"/auth/sign-up(.*)",
+	"/dashboard(.*)",
+	"api/payment",
+	"/payment(.*)",
+]);
+export default clerkMiddleware(async (auth, req) => {
 	if (!isProtectedRoutes(req)) {
-		auth.protect();
+		await auth.protect();
 	}
 });
 
