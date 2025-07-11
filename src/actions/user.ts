5d4de70c46599c57a226b16ca4fc62c08dda1ef8
ShovanDevApsis <shovan.mazumder@apsissolutions.com>
2025-07-11 10:47:10 +0600

    added

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { client } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 
 export const onAuthenticateUser = async () => {
@@ -9,7 +10,70 @@ export const onAuthenticateUser = async () => {
 		if (!user) {
 			return { status: 403 };
 		}
+
+		const userExist = await client.user.findUnique({
+			where: {
+				clerkId: user.id,
+			},
+
+			include: {
+				workspace: {
+					where: {
+						User: {
+							clerkId: user.id,
+						},
+					},
+				},
+			},
+		});
+
+		if (userExist) {
+			return { status: 200, user: userExist };
+		}
+
+		const newUser = await client.user.create({
+			data: {
+				clerkId: user.id,
+				email: user.emailAddresses[0].emailAddress,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				image: user.imageUrl,
+				studio: {
+					create: {},
+				},
+				subscription: {
+					create: {},
+				},
+				workspace: {
+					create: {
+						name: `${user.firstName}'s Workspace`,
+						type: "PERSONAL",
+					},
+				},
+			},
+			include: {
+				workspace: {
+					where: {
+						User: {
+							clerkId: user.id,
+						},
+					},
+				},
+				subscriptions: {
+					select: {
+						plan: true,
+					},
+				},
+			},
+		});
+
+		if (newUser) {
+			return { status: 201, user: newUser };
+		}
+
+		return { satus: 400 };
 	} catch (error) {
 		console.log(error);
+		return { status: 500 };
 	}
 };
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
 	// Authenticate
+	const auth = await onAuthenticateUser()
+	if(auth.status === 200 || auth.status === 200){
+		return redirect(`/dashboard/${auth.user?.firstName}${auth?.user?.lastName}`)
+	}
+
+	if(auth.status === 400 || auth.status === 500 || auth.status === 404){
+		return redirect('/auth/sign-in')
+	}
 
 	// if user already exists redirect them to right DashboardPage
 
