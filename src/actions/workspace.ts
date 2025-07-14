d312cf3b580ffc1208bd45f13c554109f1f1c105
Shovan61 <shovonmazumder61@gmail.com>
2025-07-14 08:07:48 +0600

    updated

--- src/actions/workspace.ts
+++ src/actions/workspace.ts
@@ -36,3 +36,81 @@ export const verifyAccessToWorkspace = async (workspaceID: string) => {
 		return { status: 403, data: null };
 	}
 };
+
+export const getWorkspaceFolders = async (workspaceID: string) => {
+	try {
+		const user = currentUser();
+		if (!user) return { status: 403 };
+
+		const getFolders = await client.folder.findMany({
+			where: {
+				workspaceId: workspaceID,
+			},
+			include: {
+				_count: {
+					select: {
+						videos: true,
+					},
+				},
+			},
+		});
+
+		if (getFolders && getFolders.length > 0) {
+			return { status: 200, data: getFolders };
+		}
+
+		return { status: 403, data: [] };
+	} catch (error) {
+		console.log(error);
+		return { status: 403, data: [] };
+	}
+};
+
+export const getUserVideos = async (workspaceID: string) => {
+	try {
+		const user = currentUser();
+		if (!user) return { status: 403 };
+
+		const getVideos = await client.video.findMany({
+			where: {
+				OR: [
+					{
+						workspaceId: workspaceID,
+					},
+					{
+						folderId: workspaceID,
+					},
+				],
+			},
+
+			select: {
+				id: true,
+				title: true,
+				source: true,
+				createdAt: true,
+				proccessing: true,
+				Folder: {
+					select: {
+						name: true,
+						id: true,
+					},
+				},
+				User: {
+					select: {
+						firstName: true,
+						lastName: true,
+						image: true,
+					},
+				},
+			},
+			orderBy: {
+				createdAt: "asc",
+			},
+		});
+
+		return getVideos ? { status: 200, data: getVideos } : { status: 403, data: [] };
+	} catch (error) {
+		console.log(error);
+		return { status: 403, data: [] };
+	}
+};
--- src/app/dashboard/[workspaceId]/layout.tsx
+++ src/app/dashboard/[workspaceId]/layout.tsx
@@ -1,32 +1,46 @@
 import { onAuthenticateUser } from "@/actions/user";
-import { verifyAccessToWorkspace } from "@/actions/workspace";
+import { getWorkspaceFolders, verifyAccessToWorkspace } from "@/actions/workspace";
 import { redirect } from "next/navigation";
 import React from "react";
 
+import { QueryClient } from "@tanstack/react-query";
+
 type Props = {
-  params: { workspaceId: string };
-  children: React.ReactNode;
+	params: { workspaceId: string };
+	children: React.ReactNode;
 };
 
 const Layout = async ({ params, children }: Props) => {
-  // Authenticate user first
-  const auth = await onAuthenticateUser();
-  
-  // Redirect if no user or no workspaces
-  if (!auth?.user?.workspace?.length) {
-    redirect('/auth/sign-in');
-  }
+	// Authenticate user first
+	const auth = await onAuthenticateUser();
 
-  // Verify access to the specific workspace
-  const hasAccess = await verifyAccessToWorkspace(params.workspaceId);
-  console.log(hasAccess);
-  
-  // Redirect if no access
-  if (hasAccess.status !== 200 || !hasAccess.data) {
-    redirect(`/dashboard/${auth.user.workspace[0].id}`); // Fallback to first accessible workspace
-  }
+	// Redirect if no user or no workspaces
+	if (!auth?.user?.workspace?.length) {
+		redirect("/auth/sign-in");
+	}
 
-  return <div>{children}</div>;
+	// Verify access to the specific workspace
+	const hasAccess = await verifyAccessToWorkspace(params.workspaceId);
+	console.log(hasAccess);
+
+	// Redirect if no access
+	if (hasAccess.status !== 200 || !hasAccess.data) {
+		redirect(`/dashboard/${auth.user.workspace[0].id}`); // Fallback to first accessible workspace
+	}
+
+	const query = new QueryClient();
+
+	await query.prefetchQuery({
+		queryKey: ["workspace-folders"],
+		queryFn: () => getWorkspaceFolders(params.workspaceId),
+	});
+
+	await query.prefetchQuery({
+		queryKey: ["user-videos"],
+		queryFn: () => getUserVideos(params.workspaceId),
+	});
+
+	return <div>{children}</div>;
 };
 
 export default Layout;
