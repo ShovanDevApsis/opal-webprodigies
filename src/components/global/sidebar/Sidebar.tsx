b451f02dd357c61ae5a82c71e7b048c5b39be57c
Shovan61 <shovonmazumder61@gmail.com>
2025-07-16 18:47:59 +0600

    add

--- src/components/global/sidebar/Sidebar.tsx
+++ src/components/global/sidebar/Sidebar.tsx
@@ -1,11 +1,47 @@
+import { Video } from "lucide-react";
 import React from "react";
+
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
 type Props = {
 	actionWorkspaceId: string;
 };
 
 const SidebarMain = ({ actionWorkspaceId }: Props) => {
-	return <div>SidebarMain</div>;
+	return (
+		<div className="bg-[#111111] flex-none relative p-4 h-full w-[240px] flex flex-col gap-4 items-center overflow-hidden">
+			<div className="bg-[#111111] p-4 gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0 flex">
+				<Video />
+				<span className="text-white text-xs truncate">
+					Shovans Video APP
+				</span>
+			</div>
+
+			<Select>
+				<SelectTrigger className="w-[180px]">
+					<SelectValue placeholder="Select a fruit" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectGroup>
+						<SelectLabel>Fruits</SelectLabel>
+						<SelectItem value="apple">Apple</SelectItem>
+						<SelectItem value="banana">Banana</SelectItem>
+						<SelectItem value="blueberry">Blueberry</SelectItem>
+						<SelectItem value="grapes">Grapes</SelectItem>
+						<SelectItem value="pineapple">Pineapple</SelectItem>
+					</SelectGroup>
+				</SelectContent>
+			</Select>
+		</div>
+	);
 };
 
 export default SidebarMain;
