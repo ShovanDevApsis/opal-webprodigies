171f27c830cab45ae8ecb8c9af5729ce2006c463
Shovan61 <shovonmazumder61@gmail.com>
2025-07-16 23:13:50 +0600

    update

--- src/components/global/sidebar/Sidebar.tsx
+++ src/components/global/sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Video } from "lucide-react";
 import React from "react";
 
@@ -6,32 +8,37 @@ import {
 	SelectContent,
 	SelectGroup,
 	SelectItem,
-	SelectLabel,
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import { useRouter } from "next/navigation";
 
 type Props = {
 	actionWorkspaceId: string;
 };
 
 const SidebarMain = ({ actionWorkspaceId }: Props) => {
+	const router = useRouter()
+
+	const handleChange = (val: string) => {
+	router.push(`/dashboard/${val}`)
+	};
+	
 	return (
 		<div className="bg-[#111111] flex-none relative p-4 h-full w-[240px] flex flex-col gap-4 items-center overflow-hidden">
-			<div className="bg-[#111111] p-4 gap-2 justify-center items-center mb-4 absolute top-0 left-0 right-0 flex">
+			<div className="bg-[#111111] p-4 gap-2 justify-center items-center mb-2 flex">
 				<Video />
 				<span className="text-white text-xs truncate">
 					Shovans Video APP
 				</span>
 			</div>
 
-			<Select>
+			<Select defaultValue={actionWorkspaceId} onValueChange={handleChange}>
 				<SelectTrigger className="w-[180px]">
-					<SelectValue placeholder="Select a fruit" />
+					<SelectValue placeholder="Select a workspace" />
 				</SelectTrigger>
 				<SelectContent>
 					<SelectGroup>
-						<SelectLabel>Fruits</SelectLabel>
 						<SelectItem value="apple">Apple</SelectItem>
 						<SelectItem value="banana">Banana</SelectItem>
 						<SelectItem value="blueberry">Blueberry</SelectItem>
---
+++ src/hooks/useQueryData.tsx
@@ -0,0 +1,17 @@
+import { Enabled, QueryFunction, QueryKey, useQuery } from "@tanstack/react-query";
+
+export const useQueryData = (queryKey: QueryKey, queryFn: QueryFunction, enabled: Enabled) => {
+	const { data, isFetching, isPending, isFetched, refetch } = useQuery({
+		queryKey,
+		enabled,
+		queryFn,
+	});
+
+	return {
+		data,
+		isFetching,
+		isPending,
+		isFetched,
+		refetch,
+	};
+};
