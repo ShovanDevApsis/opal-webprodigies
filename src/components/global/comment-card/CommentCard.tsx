8bcfcbe2a10395c920badf5222a071c7136f9411
Shovan61 <shovonmazumder61@gmail.com>
2025-09-01 21:17:47 +0600

    add

--- src/components/global/comment-card/CommentCard.tsx
+++ src/components/global/comment-card/CommentCard.tsx
@@ -1,9 +1,11 @@
 import { CommentRepliesProps } from "@/types/index.types";
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { User } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import CommentForm from "@/components/form/comment-form";
 
 type Props = {
 	comment: string;
@@ -15,6 +17,8 @@ type Props = {
 };
 
 function CommentCard({ author, comment, reply, videoId, commentId, isReply }: Props) {
+	const [onReply, setonReply] = useState<boolean>(false);
+
 	return (
 		<Card
 			className={cn(
@@ -38,6 +42,43 @@ function CommentCard({ author, comment, reply, videoId, commentId, isReply }: Pr
 						{author.firstname} {author.lastname}
 					</p>
 				</div>
+				<div>
+					<p className="capitalize text-sm text-neutral-400">
+						{comment}
+					</p>
+				</div>
+				{!isReply && (
+					<>
+						<div className="mt-3 flex justify-end">
+							{!onReply ? (
+								<>
+									<Button className="bg-neutral-500 hover:bg-neutral-600">
+										Reply
+									</Button>
+								</>
+							) : (
+								<>
+									<CommentForm
+										author={
+											author.firstname +
+											" " +
+											author.lastname
+										}
+										videoId={videoId}
+										commentId={
+											commentId
+										}
+										close={() =>
+											setonReply(
+												false
+											)
+										}
+									/>
+								</>
+							)}
+						</div>
+					</>
+				)}
 			</CardContent>
 		</Card>
 	);
